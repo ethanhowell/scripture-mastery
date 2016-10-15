31eea0879645d71c425dc488cedac2d111c4dd4d
Ethan J. Howell <ehowell_2010@live.com>
2016-10-15 12:06:49 -0600

    modified 2 files

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,42 @@
-$(document).ready(function() {
-	$('#answers > li').click(function(event) {
-		$(this).toggleClass('unselected');
+var grading = false;
+
+window.onload = function() {
+	bindMouseOver();
+	$('#answers').on('click', '.answer-choice.unselected', answerClicked);
+}
+
+function bindMouseOver() {
+	$('#answers').on('mouseover', '.answer-choice', function() {
+		$(this).addClass('mouse-hovering');
+		console.log($(this));
 	});
-});
+
+	$('#answers').on('mouseout', '.answer-choice', function() {
+		$(this).removeClass('mouse-hovering');
+	});
+}
+
+function answerClicked() {
+	if (!grading) {
+		grading = true;
+		$(this).toggleClass('unselected');
+		grade($(this));
+		setTimeout(prepareForNextQuestion, 2000);
+	}
+}
+
+function prepareForNextQuestion() {
+	grading = false;
+	$('.answer-choice').removeClass('incorrect').removeClass('correct').addClass('unselected');
+	loadNextQuestion();
+}
+
+function grade(answer) {
+	var remaining = $('#numberRemaining').text();
+	$('#numberRemaining').text(--remaining);
+
+	var correct = $('#numberCorrect').text();
+	if (answer.hasClass('correct'))
+		$('#numberCorrect').text(++correct);
+
+}
